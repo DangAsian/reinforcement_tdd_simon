806e5546f7378303b64103ae996517d538af2d21
Daniel Ang <dan.justin.ang@gmail.com>
2018-10-22 18:25:38 -0400

    completed the remaining tests

--- simon_says.rb
+++ simon_says.rb
@@ -5,3 +5,16 @@ end
 def shout(word)
   return word.upcase
 end
+
+def repeat(word, i)
+  new_word = "#{word} " * i
+  new_word.strip
+end
+
+def start_of_word(word, i)
+    return word.slice(0, i)
+end
+
+def first_word(word)
+  return word.split[0]
+end
